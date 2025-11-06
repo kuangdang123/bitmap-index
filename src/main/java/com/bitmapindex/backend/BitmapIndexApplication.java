80394a7cd65cfc8aabe63aced986a7722b06901f
kuangdang123 <liangjiaqicy@163.com>
2025-11-07 02:54:42 +0800

    初始化工程项目

---
+++ src/main/java/com/bitmapindex/backend/BitmapIndexApplication.java
@@ -0,0 +1,13 @@
+package com.bitmapindex.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitmapIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitmapIndexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitmapindex/backend/BitmapIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitmapindex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitmapIndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
